a93b204860f4b2cf8d2ea0e7c28d4579106ef682
Ziv Adashi <zivadashi@gmail.com>
2026-06-12 21:46:40 +0300

    sleep command

---
+++ user/sleep.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 1)
+    {
+        fprintf(2, "sleep: no argument passed\n");
+        exit(1);
+    }
+    sleep(atoi(argv[1]));
+    exit(0);
+}
