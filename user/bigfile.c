0ca7c5eaefe62eb41202bebab4fef6d177437029
Ziv Adashi <zivadashi@gmail.com>
2026-08-04 17:10:03 +0300

    bigfile logic

--- user/bigfile.c
+++ user/bigfile.c
@@ -4,23 +4,24 @@
 #include "kernel/fcntl.h"
 #include "kernel/fs.h"
 
-int
-main()
+int main()
 {
   char buf[BSIZE];
   int fd, i, blocks;
 
   fd = open("big.file", O_CREATE | O_WRONLY);
-  if(fd < 0){
+  if (fd < 0)
+  {
     printf("bigfile: cannot open big.file for writing\n");
     exit(-1);
   }
 
   blocks = 0;
-  while(1){
-    *(int*)buf = blocks;
+  while (1)
+  {
+    *(int *)buf = blocks;
     int cc = write(fd, buf, sizeof(buf));
-    if(cc <= 0)
+    if (cc <= 0)
       break;
     blocks++;
     if (blocks % 100 == 0)
@@ -28,31 +29,39 @@ main()
   }
 
   printf("\nwrote %d blocks\n", blocks);
-  if(blocks != 65803) {
+  if (blocks != 65803)
+  {
     printf("bigfile: file is too small\n");
     exit(-1);
   }
-  
+
   close(fd);
+  printf("a");
   fd = open("big.file", O_RDONLY);
-  if(fd < 0){
+  if (fd < 0)
+  {
     printf("bigfile: cannot re-open big.file for reading\n");
     exit(-1);
   }
-  for(i = 0; i < blocks; i++){
+  printf("b");
+  for (i = 0; i < blocks; i++)
+  {
     int cc = read(fd, buf, sizeof(buf));
-    if(cc <= 0){
+    printf("c");
+    if (cc <= 0)
+    {
       printf("bigfile: read error at block %d\n", i);
       exit(-1);
     }
-    if(*(int*)buf != i){
+    if (*(int *)buf != i)
+    {
       printf("bigfile: read the wrong data (%d) for block %d\n",
-             *(int*)buf, i);
+             *(int *)buf, i);
       exit(-1);
     }
   }
-
-  printf("bigfile done; ok\n"); 
+  printf("d");
+  printf("bigfile done; ok\n");
 
   exit(0);
 }
