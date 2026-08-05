a65dfd2b48c7c37be6b494f18b34c91a063750ca
Ziv Adashi <zivadashi@gmail.com>
2026-08-05 14:32:54 +0300

    temp

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -11,31 +11,33 @@
 
 void freerange(void *pa_start, void *pa_end);
 
+uint32 refcount[(PHYSTOP - KERNBASE) / 4096] = {0};
+
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
-struct run {
+struct run
+{
   struct run *next;
 };
 
-struct {
+struct
+{
   struct spinlock lock;
   struct run *freelist;
 } kmem;
 
-void
-kinit()
+void kinit()
 {
   initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  freerange(end, (void *)PHYSTOP);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
+void freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
     kfree(p);
 }
 
@@ -43,22 +45,30 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  r = (struct run *)pa;
+  int idx = (uint64)(pa - KERNBASE) / 4096;
+  if (idx < 0 || idx >= ((PHYSTOP - KERNBASE) / 4096))
+  {
+    printf("Problem!\n");
+  }
 
   acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
+  refcount[idx]--;
+  if (refcount[idx] == -1)
+  {
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+  }
   release(&kmem.lock);
 }
 
@@ -69,14 +79,23 @@ void *
 kalloc(void)
 {
   struct run *r;
+  int idx = 0;
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if (r)
+  {
+    idx = ((uint64)r - KERNBASE) / 4096;
+    if (idx < 0 || idx >= ((PHYSTOP - KERNBASE) / 4096))
+    {
+      printf("Problem!\n");
+    }
     kmem.freelist = r->next;
+    refcount[idx]++;
+  }
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  if (r)
+    memset((char *)r, 5, PGSIZE); // fill with junk
+  return (void *)r;
 }
